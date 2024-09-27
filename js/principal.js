262b12dd199dda708eb8eea9f1371e3242bbe5d5
Pedro DROPE <160151941+pedrocamargo05@users.noreply.github.com>
2024-09-27 12:12:19 -0300

    Kickoff-nutricionista

---
+++ js/principal.js
@@ -0,0 +1,16 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura)
+tdImc.textContent = imc;
+
+
